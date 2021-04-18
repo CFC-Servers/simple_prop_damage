6dd14cba4fd1661e3ee220f9a63e59a2dc077248
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2021-04-18 10:18:56 -0700

    Capitalize 2 characters
    is to Is

--- lua/entities/gmod_wire_expression2/core/custom/simple_prop_damage.lua
+++ lua/entities/gmod_wire_expression2/core/custom/simple_prop_damage.lua
@@ -1,14 +1,14 @@
 E2Lib.RegisterExtension( "simple_prop_damage", true )
 
 e2function void entity:spdAdminDisable()
-    if not isValid( this ) then return end
+    if not IsValid( this ) then return end
     if not self.player:IsAdmin() then return end
     
     this.spdDisabled = 1
 end
 
 e2function void entity:spdAdminEnable()
-    if not isValid( this ) then return end
+    if not IsValid( this ) then return end
     if not self.player:IsAdmin() then return end
     
     this.spdDisabled = 0
